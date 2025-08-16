512abe1292c60bae315164701f38e986014d3a56
Yasin Bilgin <stdxpe@gmail.com>
2025-08-16 13:24:32 +0300

    Implement: Firebase Crashlytics and Analytics

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -8,7 +12,7 @@ plugins {
 android {
     namespace = "com.yasinbilgin.case_study_clean_architecture_project"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,8 +28,8 @@ android {
         applicationId = "com.yasinbilgin.case_study_clean_architecture_project"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
